1ab9709b87c013eb73107d12852ff942d8e759c6
Wendy Wu <wendy89711@gmail.com>
2018-12-11 11:10:50 +0800

    add

--- hankjava1/src/com/tom/Poker.java
+++ hankjava1/src/com/tom/Poker.java
@@ -1,35 +1,39 @@
 package com.tom;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Poker {
-//	int[] cards = new int [52];
-	Card[] cards = new Card[52];
-	public Poker(){
-		for (int i=0; i<52; i++){
-//			cards[i] = i;
-			cards[i] = new Card(i);
+	// int[] cards = new int [52];
+	// Card[] cards = new Card[52];
+	ArrayList bag = new ArrayList();
+
+	public Poker() {
+		for (int i = 0; i < 52; i++) {
+			// cards[i] = i;
+			// cards[i] = new Card(i);
+			Card c = new Card(i);
+			bag.add(c);
 		}
+		System.out.println(bag.size());
 	}
-	public void shuffle(){
-		for (int i=0; i<52; i++){
+
+	public void shuffle() {
+		for (int i = 0; i < 52; i++) {
 			int r = new Random().nextInt(52);
-			//a = cards[i];
-			//b = cards[i];
-			Card tmp = cards[i];
-			cards[i] = cards[r];
-			cards[r] = tmp;
+			// a = cards[i];
+			// b = cards[i];
+//			Card tmp = cards[i];
+//			cards[i] = cards[r];
+//			cards[r] = tmp;
 		}
 	}
-	
-	
-	
-	
-	public void print(){
-		for (int i=0; i<52; i++){
-	
-			System.out.print(cards[i].get() + " ");
-			if(i%13 == 12)
+
+	public void print() {
+		for (int i = 0; i < 52; i++) {
+
+//			System.out.print(cards[i].get() + " ");
+			if (i % 13 == 12)
 				System.out.println();
 		}
 	}
