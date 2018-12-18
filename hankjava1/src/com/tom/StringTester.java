fc90ebc62ef5f65ff372cd89d6067454602f6a60
Wendy <wendy89711@gmail.com>
2018-12-18 11:59:30 +0800

    12/18

---
+++ hankjava1/src/com/tom/StringTester.java
@@ -0,0 +1,34 @@
+package com.tom;
+
+public class StringTester {
+
+	public static void main(String[] args) {
+		String s = "Hello world";
+		String s1 = "Hello world";
+		System.out.println(s == s1);
+		System.out.println(s.substring(3));
+		System.out.println(s.substring(0,11));
+		
+		for(int i = s.length()-1;i>=0; i--){
+			System.out.print(s.charAt(i));;
+		}
+		System.out.println();
+		System.out.println(s1.indexOf('o'));
+		System.out.println(s1.lastIndexOf('o'));
+		
+		String s2 = new String("Jack Tom Eric");
+		s2 = s2.replace("Tom", "boss");
+		System.out.println(s2);
+		
+		String name = "Tom";
+		int index = s2.indexOf(name);
+		System.out.println(index);
+		System.out.println(s2.substring(0, index)+"boss"+
+		s2.substring(index+name.length()));
+		int nn = Integer.parseInt("12");
+		float f = Float.parseFloat("45.3");
+		nn++;
+		System.out.print(nn);
+	}
+
+}
