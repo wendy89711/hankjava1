94aa4731c04be21f40c950c88153d51b51f56f26
Wendy <wendy89711@gmail.com>
2018-12-25 14:49:58 +0800

    practice

---
+++ hankjava1/src/com/tom/StringTester2.java
@@ -0,0 +1,15 @@
+package com.tom;
+
+public class StringTester2 {
+
+	public static void main(String[] args) {
+		String s = "Hello world";
+		String s1 = "Hello world";
+		String s2 = new String("Hello world");
+		System.out.println(s==s1);
+		System.out.println(s==s2);
+		
+
+	}
+
+}
