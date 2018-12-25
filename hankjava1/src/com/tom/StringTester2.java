05b0bce6bfde54b68fafe8371797dcc89d1a1989
Wendy <wendy89711@gmail.com>
2018-12-25 15:27:49 +0800

    practice2

--- hankjava1/src/com/tom/StringTester2.java
+++ hankjava1/src/com/tom/StringTester2.java
@@ -8,7 +8,15 @@ public class StringTester2 {
 		String s2 = new String("Hello world");
 		System.out.println(s==s1);
 		System.out.println(s==s2);
+		System.out.println(s.substring(2));
+		System.out.println(s2.substring(5, 11));
 		
+		
+		System.out.print("Roman alphabet: ");
+		for (char c = 'A'; c <= 'Z'; c++) {
+		System.out.print(c);
+		}
+		System.out.println();
 
 	}
 
