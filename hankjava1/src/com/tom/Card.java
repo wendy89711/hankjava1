b69899e6d7a78cd69afb6fd8a7b121f805edd582
Wendy <wendy89711@gmail.com>
2019-01-04 00:58:31 +0800

    練(半)

--- hankjava1/src/com/tom/Card.java
+++ hankjava1/src/com/tom/Card.java
@@ -3,12 +3,11 @@ package com.tom;
 public class Card {
 	char[] suits = {'\u2663','\u2666','\u2665','\u2660'};
 	int value;
-	public Card(int value){
+	public Card(int value) {
 		this.value = value;
 	}
-	
-	public String get(){
-		String s = value%13+1 + "" + suits[value/13];
+	public String get() {
+		String s = value%13+1+" "+suits[value/13];
 		return s;
 	}
 }
--- hankjava1/src/com/tom/PokerGame2.java
+++ hankjava1/src/com/tom/PokerGame2.java
@@ -3,15 +3,8 @@ package com.tom;
 public class PokerGame2 {
 
 	public static void main(String[] args) {
-		int[] n = new int[10];
-		Card[] c = new Card[52];
-		System.out.println(n[0]);
-		c[0] = new Card(7);
-		System.out.println(c[0].get());
-		
 		Poker2 poker = new Poker2();
 		poker.shuffle();
 		poker.print();
 	}
-
 }
