9f520b5033ab2620e9013cbb61e7bdfedd3d87f1
Wendy <wendy89711@gmail.com>
2019-01-07 21:06:17 +0800

    practice

--- hankjava1/src/com/tom/Card.java
+++ hankjava1/src/com/tom/Card.java
@@ -1,13 +1,13 @@
 package com.tom;
 
 public class Card {
-	char[] suits = {'\u2663','\u2666','\u2665','\u2660'};
+	char[]suits = {'\u2663','\u2666','\u2665','\u2660'};
 	int value;
 	public Card(int value) {
 		this.value = value;
 	}
 	public String get() {
-		String s = value%13+1+""+suits[value/13];
+		String s = (value%13+1+""+suits[value/13]);
 		return s;
 	}
 }
--- hankjava1/src/com/tom/PokerGame2.java
+++ hankjava1/src/com/tom/PokerGame2.java
@@ -5,14 +5,8 @@ import java.util.Random;
 public class PokerGame2 {
 
 	public static void main(String[] args) {
-		int[] p = new int[10];
-		Card[] c = new Card[52];
-		System.out.println(p[4]);
-		System.out.println(c[4]);
-		p[6] = 3;
-		System.out.println(p[6]);
-		c[31] = new Card(3);
-		System.out.println(c[31].get());
+
+
 		
 		Poker2 poker = new Poker2();
 		poker.shuffle();
