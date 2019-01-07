ee0a5a694082088bfb7cea2d48f5f317d846860c
Wendy <wendy89711@gmail.com>
2019-01-07 22:08:29 +0800

    practice

--- hankjava1/src/com/tom/StringTester2.java
+++ hankjava1/src/com/tom/StringTester2.java
@@ -3,26 +3,27 @@ package com.tom;
 public class StringTester2 {
 
 	public static void main(String[] args) {
-		String s = "Concerntrate and Fight";
-		//          0123456789012345678901
-		String s1 = "Concerntrate and Fight";
-		String s2 = new String("Concerntrate and Fight");
+		String s = "all pass !";
+//	                0123456789	
+		String s1 = "all pass !";
+		String s2 = new String("all pass !");
 		System.out.println(s==s1);
-		System.out.println(s1==s2);
-		System.out.println(s.substring(17));
-		System.out.println(s1.substring(0, 12));
-		for(int i=s2.length()-1;i>=0;i--) {
-			System.out.print(s2.charAt(i));
-		}
-		System.out.println();
-		System.out.println(s2.indexOf('e'));
-		System.out.println(s1.lastIndexOf('e'));
-		String s3 = "Cindy Wendy Judy";
-//		s3 = s3.replace("Wendy", "Good");
-//		System.out.println(s3);
-		String name = "Wendy";
-		int index = s3.indexOf(name);
-		System.out.println(index);
-		System.out.println(s3.substring(0, index)+"Good"+s3.substring(index+name.length()));
+	    System.out.println(s1==s2);
+	    System.out.println(s.substring(4));
+	    System.out.println(s1.substring(3, 8));
+	    for(int i = s2.length()-1;i>=0;i--) {
+	    	System.out.print(s2.charAt(i));
+	    }
+	    System.out.println();
+	    System.out.println(s.indexOf('a'));
+	    System.out.println(s.lastIndexOf('a'));
+	    String s3 = "Cat Dog Rabbit";
+//                   01234567890123	    
+//	    s3 = s3.replace("Dog", "Cute");
+//	    System.out.println(s3);
+	    String animal = "Dog";
+	    int index = s3.indexOf(animal);
+	    System.out.println(index);
+	    System.out.println(s3.substring(0, index)+"Cute"+s3.substring(index+animal.length()));
 	}
 }
